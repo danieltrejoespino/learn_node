fbd070e89ef892ebe21b0b90a472f19a083be152
Daniel Trejo <daniel.trejo.espino@gmail.com>
2023-10-23 19:09:32 -0600

    run

--- server_http/app.js
+++ server_http/app.js
@@ -1,7 +1,11 @@
 'use strict';
 const http = require('http');
-
-const server_ = http.createServer((req,res)=>{
-    
+const port = 3009;
+// req solicitud 
+// res respuesta
+const server_ = http.createServer((req,res)=>{    
     res.end('Hi, world');
 });
+server_.listen(port,()=>{
+    console.log(`Server listening on port ${port}`);
+})
