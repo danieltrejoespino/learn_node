0c3fa682a5cda76337b88dbb34ca2e585824dbb1
Daniel Trejo <daniel.trejo.espino@gmail.com>
2023-10-25 22:12:13 -0600

    routing

---
+++ routing/app.js
@@ -0,0 +1,46 @@
+const http= require('http');
+const courses=require('./courses')
+const PORT=3008;
+
+const server=http.createServer((req,res)=>{
+  const {method}=req;
+  switch (method) {
+    case 'GET':
+      // console.log(`Method type ${method}`);
+      console.log(`url: ${req.url} Method: ${req.method}`);
+      return handle_request_get(req,res)              
+    case 'POST':
+      console.log(`Method type ${method}`);
+      return handle_request_post(req,res)          
+    default:
+      console.log(`Method ${method} dont allow`);
+      break;
+  }
+});
+
+function handle_request_get(req,res) {
+  const path=req.url;
+  if (path==='/') {
+    res.statusCode=200;
+    res.end('Welcome :)')
+  } else if(path==='/courses'){
+    res.statusCode=200;
+    res.end(JSON.stringify(courses.info_courses))
+  } else if(path==='/courses/programming'){
+    res.statusCode=200;
+    res.end(JSON.stringify(courses.info_courses.programming))
+  }
+  else{
+    res.statusCode=400;
+    res.end('Error to find the route');
+  }
+}
+
+function handle_request_post(req,res) {
+  
+}
+
+
+server.listen(PORT,()=>{
+  console.log(`Listening in port ${PORT}`);
+})
---
+++ routing/courses.js
@@ -0,0 +1,26 @@
+let info_courses={
+    'programming':  [
+        {
+          id:1,
+          title: 'Learn js',
+          language: 'JS',
+          nivel: 'basic'
+        },
+        {
+          id:2,
+          title: 'Learn python',
+          language: 'python',
+          nivel: 'basic'
+        }
+
+    ],
+    'math': [
+      {
+        id:1,
+        title: 'fractions',
+        time: 10
+      } 
+    ]
+}
+
+module.exports.info_courses=info_courses
